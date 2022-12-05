a7bba0637540393c3fbbcf25c6c3a39d5e3a907f
Michael Ehab <michaelehab16@gmail.com>
2022-12-05 23:15:05 +0200

    Added School and Department tests (Not Passing)

--- server/test/department.test.ts
+++ server/test/department.test.ts
@@ -1,8 +1,12 @@
-import { CollegeSignUpRequest } from "@greenboard/shared";
+import {
+  CollegeSignUpRequest,
+  DepartmentSignUpRequest,
+  SchoolSignUpRequest,
+} from "@greenboard/shared";
 import supertest from "supertest";
 import { getTestServer } from "./testServer";
 
-describe("College tests", () => {
+describe("Department tests", () => {
   let client: supertest.SuperTest<supertest.Test>;
 
   const college: CollegeSignUpRequest = {
@@ -12,6 +16,21 @@ describe("College tests", () => {
     location: "Cairo, Egypt",
     foundedAt: 1952,
     adminPassword: "password",
+  };
+
+  const school: SchoolSignUpRequest = {
+    name: "Faculty of Engineering",
+    phone: "+20123456789",
+    email: "eng@cu.edu.eg",
+    adminPassword: "password",
+    collegeId: "abc",
+  };
+
+  const department: DepartmentSignUpRequest = {
+    name: "Computer Engineering",
+    email: "cmp@eng.cu.edu.eg",
+    adminPassword: "password",
+    schoolId: "abc",
   };
 
   beforeAll(async () => {
@@ -26,37 +45,7 @@ describe("College tests", () => {
     expect(result.body.jwt).toBeDefined();
   });
 
-  it("Tries adding the same college again -- /api/v1/college/signup returns 403", async () => {
-    const result = await client
-      .post("/api/v1/college/signup")
-      .send(college)
-      .expect(403);
-    expect(result.body.jwt).toBeUndefined();
-  });
-
-  it("Sends an empty object -- /api/v1/college/signup returns 400", async () => {
-    const result = await client
-      .post("/api/v1/college/signup")
-      .send({})
-      .expect(400);
-    expect(result.body.jwt).toBeUndefined();
-  });
-
-  it("Sends with a missing field -- /api/v1/college/signup returns 400", async () => {
-    const result = await client
-      .post("/api/v1/college/signup")
-      .send({
-        email: "cairouni@email.com",
-        phone: "0123456789",
-        location: "Cairo, Egypt",
-        foundedAt: 1952,
-        adminPassword: "password",
-      })
-      .expect(400);
-    expect(result.body.jwt).toBeUndefined();
-  });
-
-  it("Signs in with valid credentials -- /api/v1/college/signin returns 200", async () => {
+  it("Signs in with valid credentials from school -- /api/v1/college/signin returns 200", async () => {
     const result = await client
       .post("/api/v1/college/signin")
       .send({
@@ -67,29 +56,101 @@ describe("College tests", () => {
     expect(result.body.jwt).toBeDefined();
     expect(result.body.college.name).toEqual(college.name);
     expect(result.body.college.email).toEqual(college.email);
+    if (result.body.college.id) school.collegeId = result.body.college.id;
   });
 
-  it("Signs in with invalid password -- /api/v1/college/signin returns 403", async () => {
+  it("Adds a new valid school -- /api/v1/school/signup returns 200", async () => {
     const result = await client
-      .post("/api/v1/college/signin")
+      .post("/api/v1/school/signup")
+      .send(school)
+      .expect(200);
+    expect(result.body.jwt).toBeDefined();
+  });
+
+  it("Signs in with valid credentials -- /api/v1/school/signin returns 200", async () => {
+    const result = await client
+      .post("/api/v1/school/signin")
       .send({
-        email: college.email,
+        email: school.email,
+        password: school.adminPassword,
+      })
+      .expect(200);
+    expect(result.body.jwt).toBeDefined();
+    expect(result.body.school.name).toEqual(school.name);
+    expect(result.body.school.email).toEqual(school.email);
+    if (result.body.school.id) department.schoolId = result.body.school.id;
+  });
+
+  it("Adds a new valid department -- /api/v1/department/signup returns 200", async () => {
+    const result = await client
+      .post("/api/v1/department/signup")
+      .send(department)
+      .expect(200);
+    expect(result.body.jwt).toBeDefined();
+  });
+
+  it("Tries adding the same department again -- /api/v1/department/signup returns 403", async () => {
+    const result = await client
+      .post("/api/v1/department/signup")
+      .send(department)
+      .expect(403);
+    expect(result.body.jwt).toBeUndefined();
+  });
+
+  it("Sends an empty object -- /api/v1/department/signup returns 400", async () => {
+    const result = await client
+      .post("/api/v1/department/signup")
+      .send({})
+      .expect(400);
+    expect(result.body.jwt).toBeUndefined();
+  });
+
+  it("Sends with a missing field -- /api/v1/department/signup returns 400", async () => {
+    const result = await client
+      .post("/api/v1/department/signup")
+      .send({
+        name: "Computer Engineering",
+        adminPassword: "password",
+        schoolId: "abc",
+      })
+      .expect(400);
+    expect(result.body.jwt).toBeUndefined();
+  });
+
+  it("Signs in with valid credentials -- /api/v1/department/signin returns 200", async () => {
+    const result = await client
+      .post("/api/v1/department/signin")
+      .send({
+        email: department.email,
+        password: department.adminPassword,
+      })
+      .expect(200);
+    expect(result.body.jwt).toBeDefined();
+    expect(result.body.department.name).toEqual(department.name);
+    expect(result.body.department.email).toEqual(department.email);
+  });
+
+  it("Signs in with invalid password -- /api/v1/department/signin returns 403", async () => {
+    const result = await client
+      .post("/api/v1/department/signin")
+      .send({
+        email: department.email,
         password: "WrongPassword",
       })
       .expect(403);
     expect(result.body.jwt).toBeUndefined();
-    expect(result.body.college).toBeUndefined();
+    expect(result.body.department).toBeUndefined();
   });
 
-  it("Signs in with invalid email -- /api/v1/college/signin returns 403", async () => {
+  it("Signs in with invalid email -- /api/v1/department/signin returns 403", async () => {
     const result = await client
-      .post("/api/v1/college/signin")
+      .post("/api/v1/department/signin")
       .send({
         email: "WrongEmail",
-        password: college.adminPassword,
+        password: department.adminPassword,
       })
       .expect(403);
     expect(result.body.jwt).toBeUndefined();
-    expect(result.body.college).toBeUndefined();
+    expect(result.body.department).toBeUndefined();
   });
 });
--- server/test/school.test.ts
+++ server/test/school.test.ts
@@ -1,8 +1,8 @@
-import { CollegeSignUpRequest } from "@greenboard/shared";
+import { CollegeSignUpRequest, SchoolSignUpRequest } from "@greenboard/shared";
 import supertest from "supertest";
 import { getTestServer } from "./testServer";
 
-describe("College tests", () => {
+describe("School tests", () => {
   let client: supertest.SuperTest<supertest.Test>;
 
   const college: CollegeSignUpRequest = {
@@ -14,11 +14,19 @@ describe("College tests", () => {
     adminPassword: "password",
   };
 
+  const school: SchoolSignUpRequest = {
+    name: "Faculty of Engineering",
+    phone: "+20123456789",
+    email: "eng@cu.edu.eg",
+    adminPassword: "password",
+    collegeId: "abc",
+  };
+
   beforeAll(async () => {
     client = await getTestServer();
   });
 
-  it("Adds a new valid college -- /api/v1/college/signup returns 200", async () => {
+  it("Adds a new valid college from school -- /api/v1/college/signup returns 200", async () => {
     const result = await client
       .post("/api/v1/college/signup")
       .send(college)
@@ -26,37 +34,7 @@ describe("College tests", () => {
     expect(result.body.jwt).toBeDefined();
   });
 
-  it("Tries adding the same college again -- /api/v1/college/signup returns 403", async () => {
-    const result = await client
-      .post("/api/v1/college/signup")
-      .send(college)
-      .expect(403);
-    expect(result.body.jwt).toBeUndefined();
-  });
-
-  it("Sends an empty object -- /api/v1/college/signup returns 400", async () => {
-    const result = await client
-      .post("/api/v1/college/signup")
-      .send({})
-      .expect(400);
-    expect(result.body.jwt).toBeUndefined();
-  });
-
-  it("Sends with a missing field -- /api/v1/college/signup returns 400", async () => {
-    const result = await client
-      .post("/api/v1/college/signup")
-      .send({
-        email: "cairouni@email.com",
-        phone: "0123456789",
-        location: "Cairo, Egypt",
-        foundedAt: 1952,
-        adminPassword: "password",
-      })
-      .expect(400);
-    expect(result.body.jwt).toBeUndefined();
-  });
-
-  it("Signs in with valid credentials -- /api/v1/college/signin returns 200", async () => {
+  it("Signs in with valid credentials from school -- /api/v1/college/signin returns 200", async () => {
     const result = await client
       .post("/api/v1/college/signin")
       .send({
@@ -67,29 +45,80 @@ describe("College tests", () => {
     expect(result.body.jwt).toBeDefined();
     expect(result.body.college.name).toEqual(college.name);
     expect(result.body.college.email).toEqual(college.email);
+    if (result.body.college.id) school.collegeId = result.body.college.id;
   });
 
-  it("Signs in with invalid password -- /api/v1/college/signin returns 403", async () => {
+  it("Adds a new valid school -- /api/v1/school/signup returns 200", async () => {
     const result = await client
-      .post("/api/v1/college/signin")
+      .post("/api/v1/school/signup")
+      .send(school)
+      .expect(200);
+    expect(result.body.jwt).toBeDefined();
+  });
+
+  it("Tries adding the same school again -- /api/v1/school/signup returns 403", async () => {
+    const result = await client
+      .post("/api/v1/school/signup")
+      .send(school)
+      .expect(403);
+    expect(result.body.jwt).toBeUndefined();
+  });
+
+  it("Sends an empty object -- /api/v1/school/signup returns 400", async () => {
+    const result = await client
+      .post("/api/v1/school/signup")
+      .send({})
+      .expect(400);
+    expect(result.body.jwt).toBeUndefined();
+  });
+
+  it("Sends with a missing field -- /api/v1/college/signup returns 400", async () => {
+    const result = await client
+      .post("/api/v1/school/signup")
       .send({
-        email: college.email,
+        name: "Faculty of Engineering",
+        phone: "+20123456789",
+        email: "eng@cu.edu.eg",
+        collegeId: "abc",
+      })
+      .expect(400);
+    expect(result.body.jwt).toBeUndefined();
+  });
+
+  it("Signs in with valid credentials -- /api/v1/school/signin returns 200", async () => {
+    const result = await client
+      .post("/api/v1/school/signin")
+      .send({
+        email: school.email,
+        password: school.adminPassword,
+      })
+      .expect(200);
+    expect(result.body.jwt).toBeDefined();
+    expect(result.body.school.name).toEqual(school.name);
+    expect(result.body.school.email).toEqual(school.email);
+  });
+
+  it("Signs in with invalid password -- /api/v1/school/signin returns 403", async () => {
+    const result = await client
+      .post("/api/v1/school/signin")
+      .send({
+        email: school.email,
         password: "WrongPassword",
       })
       .expect(403);
     expect(result.body.jwt).toBeUndefined();
-    expect(result.body.college).toBeUndefined();
+    expect(result.body.school).toBeUndefined();
   });
 
-  it("Signs in with invalid email -- /api/v1/college/signin returns 403", async () => {
+  it("Signs in with invalid email -- /api/v1/school/signin returns 403", async () => {
     const result = await client
-      .post("/api/v1/college/signin")
+      .post("/api/v1/school/signin")
       .send({
         email: "WrongEmail",
-        password: college.adminPassword,
+        password: school.adminPassword,
       })
       .expect(403);
     expect(result.body.jwt).toBeUndefined();
-    expect(result.body.college).toBeUndefined();
+    expect(result.body.school).toBeUndefined();
   });
 });
