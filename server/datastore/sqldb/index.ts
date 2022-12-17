68a0e83836ac143947f98d2a4c7694ec4929532b
Michael Ehab <michaelehab16@gmail.com>
2022-12-17 21:10:30 +0200

    Fixed Merging issue

--- server/datastore/sqldb/index.ts
+++ server/datastore/sqldb/index.ts
@@ -390,34 +390,6 @@ export class SQLDataStore implements DataStore {
       PostId
     );
   }
-
-  async createInstructorAnswer(
-    InstructorAnswer: InstructorAnswer
-  ): Promise<void> {
-    await this.createComment(InstructorAnswer);
-    await this.db.run(
-      "INSERT INTO instructors_answers(id,instructorId,questionId) VALUES (?,?,?)",
-      InstructorAnswer.id,
-      InstructorAnswer.instructorId,
-      InstructorAnswer.questionId
-    );
-  }
-  async getInstructorAnsweById(
-    AnswerId: string
-  ): Promise<InstructorAnswer | undefined> {
-    return await this.db.get<InstructorAnswer>(
-      "SELECT * FROM comments JOIN instructors_answers ON instructors_answers.id = comments.id WHERE comments.id = ?",
-      AnswerId
-    );
-  }
-  async listInstructorAnswerByPostId(
-    questionId: string
-  ): Promise<InstructorAnswer[]> {
-    return await this.db.all<InstructorAnswer[]>(
-      "SELECT * FROM comments JOIN instructors_answers ON instructors_answers.id = comments.id WHERE questionId=?",
-      questionId
-    );
-  }
   async createInstructorAnswer(
     InstructorAnswer: InstructorAnswer
   ): Promise<void> {
