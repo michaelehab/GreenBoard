8dfa6580f51c6fb1460b917459a147e12a47f800
Michael Ehab <michaelehab16@gmail.com>
2022-12-18 13:58:14 +0200

    Added Take Assessment Feature tests (not passing)

--- server/datastore/dao/gradesDao.ts
+++ server/datastore/dao/gradesDao.ts
@@ -2,5 +2,5 @@ import { Grade } from "@greenboard/shared";
 
 export interface GradesDao {
   createGrade(grade: Grade): Promise<void>;
-  getGrades(studentId: string, quizId: string): Promise<Grade | undefined>;
+  getGrade(studentId: string, quizId: string): Promise<Grade | undefined>;
 }
