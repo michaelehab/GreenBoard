0395a696945560ca5e9d0a1a93e33997479b5224
Michael Ehab <michaelehab16@gmail.com>
2022-12-27 15:59:46 +0200

    Updated studentQuestion Card

--- web/src/components/studentQuestionCard.tsx
+++ web/src/components/studentQuestionCard.tsx
@@ -1,6 +1,16 @@
-import { Text, Center, Box, Link as ChakraLink } from "@chakra-ui/react";
+import {
+  Text,
+  Center,
+  Box,
+  Link as ChakraLink,
+  Avatar,
+  WrapItem,
+  Stack,
+  Flex,
+} from "@chakra-ui/react";
 import { Post, UserDataAndPost } from "@greenboard/shared";
 import { Link } from "react-router-dom";
+import { format } from "timeago.js";
 
 export const StudentQuestionCard: React.FC<UserDataAndPost> = (question) => {
   return (
@@ -14,15 +24,32 @@ export const StudentQuestionCard: React.FC<UserDataAndPost> = (question) => {
         rounded="md"
         bg="white"
       >
-        <Text fontSize="md" fontWeight="bold">
-            {question.firstName} {question.lastName}
-          </Text>
-        <Link to={`/courses/${question.courseId}/questions/${question.id}`}>
-          <Text fontSize="md" fontWeight="bold">
-            {question.title}
-          </Text>
-        </Link>
-        <ChakraLink href={question.url}>Link</ChakraLink>
+        <Flex gap={2}>
+          <Link to={`/courses/${question.courseId}/questions/${question.id}`}>
+            <Text fontSize="md" fontWeight="bold">
+              {question.title}
+            </Text>
+          </Link>
+          {question.url !== "NoLink" && (
+            <ChakraLink color="#4d7e3e" href={question.url}>
+              Visit Link
+            </ChakraLink>
+          )}
+        </Flex>
+        <Flex justifyContent="space-between">
+          <Stack direction="row">
+            <WrapItem>
+              <Avatar
+                size={"xs"}
+                name={`${question.firstName} ${question.lastName}`}
+              />
+            </WrapItem>
+            <Text fontSize="md">
+              {question.firstName} {question.lastName}
+            </Text>
+          </Stack>
+          <Text color="#4d7e3e">{format(question.postedAt, "en_US")}</Text>
+        </Flex>
       </Box>
     </Center>
   );
