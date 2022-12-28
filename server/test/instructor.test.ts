545828d758573a5ccf40965eff7ed77b8c5baad6
Ibraam Nashaat <ibraam.nashaat2@gmail.com>
2022-12-28 16:24:54 +0200

    Instructor update routes working

--- server/test/instructor.test.ts
+++ server/test/instructor.test.ts
@@ -1,10 +1,14 @@
 import { InstructorSignUpRequest } from "@greenboard/shared";
 import supertest from "supertest";
 import { SEED_DEPARTMENT } from "../datastore/sqldb/seeds";
-import { getTestServer } from "./testUtils";
+import { getAuthToken, getTestServer } from "./testUtils";
 
 describe("Instructor tests", () => {
   let client: supertest.SuperTest<supertest.Test>;
+  const newFirstName: string = "updatedFirstName";
+  const newLastName: string = "updatedLastName";
+  const newPhone: string = "updatedPhone";
+  const newEmail: string = "Email@google.com";
 
   const instructor: InstructorSignUpRequest = {
     email: "test@outlook.com",
@@ -91,6 +95,116 @@ describe("Instructor tests", () => {
       })
       .expect(403);
     expect(result.body.jwt).toBeUndefined();
+    expect(result.body.instructor).toBeUndefined();
+  });
+  it("Updates signed in instructor Name -- PUT /api/v1/instructor/update returns 200", async () => {
+    const result = await client
+      .put("/api/v1/instructor/update")
+      .send({
+        firstName: newFirstName,
+        lastName: newLastName,
+      })
+      .set(
+        await getAuthToken(
+          "/api/v1/instructor/signin",
+          instructor.email,
+          instructor.password
+        )
+      )
+      .expect(200);
+    expect(result.body.instructor).toBeDefined();
+    expect(result.body.instructor.firstName).toEqual(newFirstName);
+    expect(result.body.instructor.lastName).toEqual(newLastName);
+    expect(result.body.instructor.email).toEqual(instructor.email);
+    expect(result.body.instructor.phoneNumber).toEqual(instructor.phoneNumber);
+  });
+
+  it("Updates signed in instructor not signed in -- PUT /api/v1/instructor/update returns 403", async () => {
+    const result = await client
+      .put("/api/v1/instructor/update")
+      .send({
+        firstName: newFirstName,
+      })
+      .expect(401);
+    expect(result.body.instructor).toBeUndefined();
+  });
+
+  it("Updates signed in instructor name with empty -- PUT /api/v1/instructor/update returns 400", async () => {
+    const result = await client
+      .put("/api/v1/instructor/update")
+      .send({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phoneNumber: "",
+      })
+      .set(
+        await getAuthToken(
+          "/api/v1/instructor/signin",
+          instructor.email,
+          instructor.password
+        )
+      )
+      .expect(400);
+    expect(result.body.instructor).toBeUndefined();
+  });
+
+  it("Updates signed in instructor email -- PUT /api/v1/instructor/update returns 200", async () => {
+    const result = await client
+      .put("/api/v1/instructor/update")
+      .send({
+        email: newEmail,
+      })
+      .set(
+        await getAuthToken(
+          "/api/v1/instructor/signin",
+          instructor.email,
+          instructor.password
+        )
+      )
+      .expect(200);
+    expect(result.body.instructor).toBeDefined();
+    expect(result.body.instructor.firstName).toEqual(newFirstName);
+    expect(result.body.instructor.lastName).toEqual(newLastName);
+    expect(result.body.instructor.email).toEqual(newEmail);
+    expect(result.body.instructor.phoneNumber).toEqual(instructor.phoneNumber);
+  });
+
+  it("Updates signed in instructor phoneNumber -- PUT /api/v1/instructor/update returns 200", async () => {
+    const result = await client
+      .put("/api/v1/instructor/update")
+      .send({
+        phoneNumber: newPhone,
+      })
+      .set(
+        await getAuthToken(
+          "/api/v1/instructor/signin",
+          newEmail,
+          instructor.password
+        )
+      )
+      .expect(200);
+    expect(result.body.instructor).toBeDefined();
+    expect(result.body.instructor.lastName).toEqual(newLastName);
+    expect(result.body.instructor.firstName).toEqual(newFirstName);
+    expect(result.body.instructor.email).toEqual(newEmail);
+    expect(result.body.instructor.phoneNumber).toEqual(newPhone);
+  });
+
+  it("Updates signed in instructor phoneNumber with empty -- PUT /api/v1/instructor/update returns 400", async () => {
+    const result = await client
+      .put("/api/v1/instructor/update")
+      .send({
+        phoneNumber: "",
+      })
+      .set(
+        await getAuthToken(
+          "/api/v1/instructor/signin",
+          newEmail,
+          instructor.password
+        )
+      )
+      .expect(400);
     expect(result.body.instructor).toBeUndefined();
   });
 });
